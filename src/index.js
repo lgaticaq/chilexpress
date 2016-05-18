d2e05c38af40b91124ffc649cbcdb8da13cb3508
Leonardo Gatica <lgatica@protonmail.com>
2016-05-18 13:16:19 -0400

    Reverse last changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const moment = require('moment');
 
 module.exports = orderId => {
   const options = {
@@ -12,51 +11,42 @@ module.exports = orderId => {
     rejectUnauthorized: false,
     transform: cheerio.load
   };
-  const data = {orderId: orderId};
   return rp(options).then($ => {
-    const resume = $('#ListaOTHijas > tr:nth-child(1) td').get().map(i => $(i).text());
-    if (resume.length === 0) throw new Error('Not found order id');
-    data.transportId = resume[0];
-    data.status = resume[1];
-    data.delivery = {
-      name: resume[2],
-      datetime: moment(`${resume[3]} -03:00`, 'DD/MM/YYYY HH:mm ZZ').toDate()
+    const tbl = $('.addresses > tbody').get().map(row => {
+      return $(row).find('td').get().map(cell => $(cell).html());
+    });
+    if (tbl.length === 0) throw new Error('Not found order id');
+    const resume = $('.wigdet-content > ul > li > ul > li').get().map(i => {
+      const h = $(i);
+      h.find('strong').remove();
+      return h.html();
+    });
+    const data = {
+      orderId: resume[0],
+      product: resume[1],
+      service: resume[2],
+      status: resume[3],
+      isDeliveried: false
     };
-    data.isDeliveried = resume[1] === 'DESCARGADA';
-    data.receptor = {rut: null, name: null};
-    const options = {
-      method: 'POST',
-      url: 'https://www.chilexpress.cl/Views/ChilexpressCL/Resultado-busqueda.aspx/ObtieneTrakingWEBM',
-      body: JSON.stringify({ot: resume[0], Filas: 0, Hoja: 0}),
-      rejectUnauthorized: false,
-      headers: {
-        'Origin': 'https://www.chilexpress.cl',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2734.0 Safari/537.36',
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Referer': `https://www.chilexpress.cl/Views/ChilexpressCL/Resultado-busqueda.aspx?DATA=${orderId}`,
-        'X-Requested-With': 'XMLHttpRequest',
-        'Connection': 'keep-alive'
-      },
-      transform: body => {
-        body = JSON.parse(body);
-        return cheerio.load(body.d);
-      }
-    };
-    return rp(options);
-  }).then($ => {
-    const info = $('.detaLista ol li').get().map(i => $(i).text().trim().split(/:\s+/));
-    for (let i of info) {
-      if (i[0] === 'Producto') data.product = i[1];
-      if (i[0] === 'Servicio') data.service = i[1];
-      if (i[0] === 'Rut Receptor') data.receptor.rut = i[1];
-      if (i[0] === 'Nombre Receptor') data.receptor.name = i[1];
+    const pattern = /(\d{2})\/(\d{2})\/(\d{4})(\d{2})\:(\d{2})/;
+    const downloadHead = $('h4:contains("Datos de Descarga")');
+    if (downloadHead.length > 0) {
+      const download = downloadHead.parents('.wigdet-content').find('ul > li > ul > li').get().map(i => {
+        const h = $(i);
+        h.find('strong').remove();
+        h.find('a').remove();
+        return h.html().replace('&nbsp;', '').replace('()', '').trim();
+      });
+      data.receptor = {rut: download[0], name: download[3]};
+      const dst = `${download[1]}${download[2]}`;
+      data.delivery = new Date(dst.replace(pattern,'$3-$2-$1T$4:$5:00-03:00'));
+      data.isDeliveried = true;
     }
-    data.history = $('.detaLista #ListaTrackingOT').get().map(y => {
-      const d = $(y).find('td').get().map(x => $(x).text());
+    data.history = tbl.map(x => {
+      const st = `${x[0]}${x[1]}`;
       return {
-        datetime: moment(`${d[0]} ${d[1]} -03:00`, 'DD/MM/YYYY HH:mm ZZ').toDate(),
-        activity: d[2]
+        datetime: new Date(st.replace(pattern,'$3-$2-$1T$4:$5:00-03:00')),
+        activity: x[2]
       };
     });
     return data;
--- test/test.js
+++ test/test.js
@@ -11,8 +11,7 @@ describe('chilexpress', () => {
 
   describe('valid order id', () => {
 
-    const orderId = '11111111111';
-    const transportId = '22222222222';
+    const orderId = '111111111111';
 
     beforeEach(() => {
       nock.disableNetConnect();
@@ -20,15 +19,12 @@ describe('chilexpress', () => {
         .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
         .query({DATA: orderId})
         .replyWithFile(200, path.join(__dirname, 'valid.html'));
-      nock('https://www.chilexpress.cl')
-        .post('/Views/ChilexpressCL/Resultado-busqueda.aspx/ObtieneTrakingWEBM', JSON.stringify({ot: transportId, Filas: 0, Hoja: 0}))
-        .replyWithFile(200, path.join(__dirname, 'valid2.html'));
     });
 
     it('should return a array statuses', done => {
       lib(orderId).then(data => {
         expect(data).to.be.a('object');
-        expect(data).to.include.keys('orderId', 'transportId', 'product', 'service', 'status', 'isDeliveried', 'history');
+        expect(data).to.include.keys('orderId', 'product', 'service', 'status', 'isDeliveried', 'history');
         for (let history of data.history) {
           expect(history).to.include.keys('datetime', 'activity');
           expect(history.datetime).to.be.a('date');
