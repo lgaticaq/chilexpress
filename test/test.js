e979d2a09ae952511a42f83d1ee9c4e3875f4269
Leonardo Gatica <lgatica@protonmail.com>
2016-12-18 00:47:40 -0300

    chore(tests): Complete tests

--- test/test.js
+++ test/test.js
@@ -10,19 +10,42 @@ const lib = require('../src');
 describe('chilexpress', () => {
 
   describe('valid order id', () => {
-
-    const orderId = '111111111111';
-
     beforeEach(() => {
       nock.disableNetConnect();
       nock('https://www.chilexpress.cl')
         .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
-        .query({DATA: orderId})
+        .query({DATA: '111111111111'})
         .replyWithFile(200, path.join(__dirname, 'valid.html'));
     });
 
     it('should return a array statuses', done => {
-      lib(orderId).then(data => {
+      lib('111111111111').then(data => {
+        expect(data).to.be.a('object');
+        expect(data).to.include.keys('orderId', 'product', 'service', 'status', 'isDeliveried', 'history');
+        for (let history of data.history) {
+          expect(history).to.include.keys('datetime', 'activity');
+          expect(history.datetime).to.be.a('date');
+          expect(history.activity).to.be.a('string');
+        }
+        done();
+      }).catch(err => {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+  });
+
+  describe('valid order id with delivery', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      nock('https://www.chilexpress.cl')
+        .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
+        .query({DATA: '111111111111'})
+        .replyWithFile(200, path.join(__dirname, 'delivery.html'));
+    });
+
+    it('should return a array statuses', done => {
+      lib('111111111111').then(data => {
         expect(data).to.be.a('object');
         expect(data).to.include.keys('orderId', 'product', 'service', 'status', 'isDeliveried', 'history');
         for (let history of data.history) {
@@ -39,23 +62,54 @@ describe('chilexpress', () => {
   });
 
   describe('invalid order id', () => {
-
-    const orderId = '1';
-
     beforeEach(() => {
       nock.disableNetConnect();
       nock('https://www.chilexpress.cl')
         .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
-        .query({DATA: orderId})
+        .query({DATA: '1'})
         .replyWithFile(200, path.join(__dirname, 'invalid.html'));
     });
 
-    it('should return a error', done => {
-      lib(orderId).then((data) => {
+    it('should return an error', done => {
+      lib('1').then((data) => {
         expect(data).to.be.undefined;
         done();
       }).catch(err => {
         expect(err).to.eql(new Error('Not found order id'));
+        done();
+      });
+    });
+  });
+
+  describe('server error', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      nock('https://www.chilexpress.cl')
+        .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
+        .query({DATA: '1'})
+        .replyWithError('Server error');
+    });
+
+    it('should return an error', done => {
+      lib('1').catch(err => {
+        expect(err).to.be.an('error');
+        done();
+      });
+    });
+  });
+
+  describe('bad status code', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      nock('https://www.chilexpress.cl')
+        .get('/Views/ChilexpressCL/Resultado-busqueda.aspx')
+        .query({DATA: '1'})
+        .reply(301);
+    });
+
+    it('should return an error', done => {
+      lib('1').catch(err => {
+        expect(err).to.be.an('error');
         done();
       });
     });
